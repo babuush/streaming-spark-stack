fec06995dd7ad6b9067fa1915e22307a3eda40aa
babuush <thanapatpiyamassakul@gmail.com>
2025-08-12 00:37:20 +0700

    fix: remove user tag

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,19 +2,19 @@
 set -e
 
 if [ -e "/opt/airflow/requirements.txt" ]; then
-  $(command python) pip install --upgrade pip
-  $(command -v pip) install --user -r requirements.txt
+    $(command python) pip install --upgrade pip
+    $(command -v pip) install -r requirements.txt
 fi
 
 if [ ! -f "/opt/airflow/airflow.db" ]; then
-  airflow db init && \
-  airflow users create \
-    --username admin \
-    --firstname admin \
-    --lastname admin \
-    --role Admin \
-    --email admin@example.com \
-    --password admin
+    airflow db init &&
+        airflow users create \
+            --username admin \
+            --firstname admin \
+            --lastname admin \
+            --role Admin \
+            --email admin@example.com \
+            --password admin
 fi
 
 $(command -v airflow) db upgrade
