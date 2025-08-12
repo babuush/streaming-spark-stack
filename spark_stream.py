52677aab9ba81a008b0f4b29835e7c07d2683088
babuush <thanapatpiyamassakul@gmail.com>
2025-08-13 00:37:54 +0700

    feat: add core functions structure

---
+++ spark_stream.py
@@ -0,0 +1,30 @@
+# import logging
+#
+# from cassandra.cluster import Cluster
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import from_json, col
+# from pyspark.sql.types import StructType, StructField, StringType
+
+
+def create_keyspace(session):
+    pass
+
+
+def create_table(session):
+    pass
+
+
+def insert_data(session, **kwargs):
+    pass
+
+
+def create_spark_connection():
+    pass
+
+
+def create_cassandra_connection():
+    pass
+
+
+if __name__ == "__main__":
+    spark_conn = create_spark_connection()
