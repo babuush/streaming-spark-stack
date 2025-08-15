12ba78d055f9de9d675b120dd5b494e5ab84bd7b
babuush <thanapatpiyamassakul@gmail.com>
2025-08-15 12:51:21 +0700

    feat: add create_spark_connection function

--- spark_stream.py
+++ spark_stream.py
@@ -1,7 +1,9 @@
-# import logging
+import logging
+
 #
 # from cassandra.cluster import Cluster
-# from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
+
 # from pyspark.sql.functions import from_json, col
 # from pyspark.sql.types import StructType, StructField, StringType
 
@@ -19,7 +21,26 @@ def insert_data(session, **kwargs):
 
 
 def create_spark_connection():
-    pass
+    s_conn = None
+
+    try:
+        s_conn = (
+            SparkSession.builder.appName("SparkDataStreaming")
+            .config(
+                "spark.jars.packages",
+                "com.datastax.spark:spark-cassandra-connector_2.13:3.41",
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1",
+            )
+            .config("spark.cassandra.connection.host", "localhost")
+            .getOrCreate()
+        )
+
+        s_conn.sparkContext.setLogLevel("Error")
+        logging.info("Spark connection create successfully")
+    except Exception as e:
+        logging.error(f"Couldn't create the spark session due to exception {e}")
+
+    return s_conn
 
 
 def create_cassandra_connection():
